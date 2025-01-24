e597a2a40924bfdbfc11d11d53719e4185ef8689
Adam Mos <moztrom93@gmail.com>
2025-01-24 21:35:44 +0100

    added Haskell solution for day1

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -5,33 +5,30 @@ list2 = []
 
 
 def readFile():
-    with open('puzzleInput.txt', 'r') as file:
+    with open("puzzleInput.txt", "r") as file:
         for line in file:
             num1, num2 = map(int, line.split())
             list1.append(num1)
             list2.append(num2)
 
 
-
-
 def compare(list1, list2):
     longest = max(len(list1), len(list2))
     result = []
     i = 0
-    while( i < longest):
-        if(list1[i] >=  list2[i]):
+    while i < longest:
+        if list1[i] >= list2[i]:
             result.append(list1[i] - list2[i])
         else:
             result.append(list2[i] - list1[i])
         i = i + 1
-    
-    return result
 
+    return result
 
 
 def run():
     readFile()
     list1.sort()
     list2.sort()
-    result = compare(list1,list2)
+    result = compare(list1, list2)
     return sum(result)
